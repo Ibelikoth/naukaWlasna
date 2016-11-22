2ca3aac3ea4fe37a41aa004d4158b7cefad18fe2
Ibelikoth <pawelorli@gmail.com>
2016-11-22 22:47:58 +0100

    dodane wskazniki

--- c++/1. Hello World/main/main.cpp
+++ c++/1. Hello World/main/main.cpp
@@ -38,10 +38,59 @@ bool accept2(){
         return false;
     }
 }
+bool accept3(){
+    int tries = 1;
+    while (tries<4) {
+        cout<<"Czy chcesz kontynuowac? (t lub n)\n";
+        char answer = 0;
+        cin>>answer;
+
+        switch(answer){
+        case 't':
+            return true;
+        case 'n':
+            return false;
+        default:
+            cout<<"Niestety, nie zrozumialem.\n";
+            ++tries;
+        }
+    }
+    cout<<"Chyba raczej nie.\n";
+    return false;
+}
+void copyFCT(){
+    int v1[10] = {0,1,2,3,4,5,6,7,8,9};
+    int v2[10];
+
+    for(int i=0; i!=10; i++)
+        v2[i]=v1[i];
+}
+void printA(){
+    int v[] = {0,1,2,3,4,5,6,7,8,9};
+
+    for(int x : v)//dla kazdego x w v
+        cout<< x << "\n";
+}
+void increment(){
+    int v[] = {0,1,2,3,4,5,6,7,8,9};
+    for(int& x : v){
+        ++x;
+        cout<<x;
+    }
+}
+int countX(char*p, char x){
+    if(p==nullptr) return 0;
+    int count = 0;
+    for(;*p!=0; ++p)
+        if(*p==x)
+            ++count;
+    return count;
+}
 
 int main()
 {
-    //print_square(1.234);
+    /*
+    print_square(1.234);
     double d1 = 2.3;
     double d2 {2.3};
 
@@ -50,7 +99,7 @@ int main()
     complex<double> z3 = {1.2};
     vector<int> v {1,2,3,4,5,6};
 
-    /*
+
     cout<<"d1 : "<<d1<<" zajmuje miejsca : "<<sizeof(d1)<<endl;
     cout<<"d2 : "<<d2<<" zajmuje miejsca : "<<sizeof(d2)<<endl;
     cout<<"z1 : "<<z1<<" zajmuje miejsca : "<<sizeof(z1)<<endl;
@@ -64,7 +113,21 @@ int main()
     auto d = 1.2;
     auto z = sqrt(y);
 
-    cout<<accept2();
-    */
+    cout<<accept3();
+
+    char v[6];
+    char*p=&v[3];//wskaznik na czwarty element tablicy
+    char x = *p;//*p jest obiektem, na który wskazuje p
+
+    printA();
+    increment();
+
+    int a[n];//tablica intow 'a' o 'n' wartosci
+    int*p;//wskaznik na intach
+    int&a//referencja na tablice a
+    double *pd = nullptr;
+    Link<Record>*lst = nullptr;//wskaźnik do łącza rekordu
+    int x = nullptr;//error nullptr jest wskaznikiem a nie liczba*/
+
     return 0;
 }
